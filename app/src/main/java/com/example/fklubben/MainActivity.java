5a72a5b29ca944a3f94f50b9be4735a352a664bd
Dazasaur <mikkel0_0@live.dk>
2020-03-04 15:28:55 +0100

    kursusgang 04-03
    nyt

--- app/src/main/java/com/example/fklubben/MainActivity.java
+++ app/src/main/java/com/example/fklubben/MainActivity.java
@@ -2,19 +2,52 @@ package com.example.fklubben;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    final Button aboutUsButton = (Button) findViewById(R.id.btn_aboutus);
+    final Button mapButton = (Button) findViewById(R.id.btn_map);
+    final Button drinksButton = (Button) findViewById(R.id.btn_drinks);
+    final Button contactButton = (Button) findViewById(R.id.btn_kontakt);
+    final Button testButton = (Button) findViewById(R.id.btn_test);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("OnCreate", "OnCreate called!");
+
     }
 
+    public void onClickAboutUs(View view){
+        Intent intent = new Intent(this, AboutUs.class);
+        startActivity(intent);
+    }
+
+    public void onClickDrinks(View view){
+        Intent intent = new Intent(this, drinks.class);
+        startActivity(intent);
+    }
+
+    public void onClickContact(View view){
+        Intent intent = new Intent(this, contact.class);
+        startActivity(intent);
+    }
+
+    public void onClickMap(View view){
+        Intent intent = new Intent(this, map.class);
+        startActivity(intent);
+    }
+
+    public void onClickTest(View view){
+        Intent intent = new Intent(this, test.class);
+        startActivity(intent);
+    }
     protected void onStart()
     {
         super.onStart();
@@ -45,4 +78,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i("onDestroy", "onDestroy called!");
     }
+
 }
