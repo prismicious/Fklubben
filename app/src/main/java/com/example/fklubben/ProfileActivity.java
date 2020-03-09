c451a6c78db949c697e3aa5314bae64ceab30ebb
Dazasaur <mikkel.sahlholdt@gmail.com>
2020-03-09 22:02:04 +0100

    Nu virker det
    Profile Activity virker

--- app/src/main/java/com/example/fklubben/ProfileActivity.java
+++ app/src/main/java/com/example/fklubben/ProfileActivity.java
@@ -3,25 +3,30 @@ package com.example.fklubben;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    String put_firstName = String.valueOf((TextView)findViewById(R.id.firstName));
-    String put_lastName = String.valueOf((TextView)findViewById(R.id.lastName));
-    String put_Email = String.valueOf((TextView)findViewById(R.id.email));
+    private static final int CAMERA_REQUEST = 1888;
+
     //ImageView imageView = findViewById(R.id.profileImg);
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -29,34 +34,70 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        SharedPreferences myPrefsFile = getSharedPreferences("MyPrefsFile", Activity.MODE_PRIVATE);
+
+        String savedFirstName= myPrefsFile.getString("firstname", "");
+        String savedLastName = myPrefsFile.getString("lastname", "");
+        String savedEmail = myPrefsFile.getString("email", "");
+
+        EditText firstname = (EditText) findViewById(R.id.firstName);
+        EditText lastname = (EditText) findViewById(R.id.lastName);
+        EditText email = (EditText) findViewById(R.id.email);
+
+        firstname.setText("" + savedFirstName);
+        lastname.setText("" + savedLastName);
+        email.setText("" + savedEmail);
     }
 
-    public void onClickCamera(View view){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+    public void onClickCamera(View  view){
+        Intent myCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(myCameraIntent, CAMERA_REQUEST);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            //imageView.setImageBitmap(imageBitmap);
-        }
+    public void onActivityResult(int reqCode, int resCode, Intent data){
+        super.onActivityResult(reqCode, resCode, data);
+        switch (reqCode){
+            case CAMERA_REQUEST : {
+                if(resCode == Activity.RESULT_OK){
+                    Bitmap newFace = (Bitmap) data.getExtras().get("data");
+
+                    ImageView image = (ImageView) findViewById(R.id.profileImg);
+                    image.setImageBitmap(newFace);
+
+                    ContextWrapper cw = new ContextWrapper(getApplicationContext());
+                    File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+                    File mypath = new File(directory,"profile.jpg");
+                    FileOutputStream outputstream = null;
+
+                    try{
+                        outputstream = new FileOutputStream(mypath);
+                        newFace.compress(Bitmap.CompressFormat.JPEG, 90, outputstream);
+                        outputstream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace(); }
+
+
+                } }
+            default: break; }
     }
 
     public void onClickSave(View view){
-      //  savePrefs(view);
+       savePrefs(view);
     }
- /*
+
     public void savePrefs(View view) {
+
+        EditText firstname = (EditText) findViewById(R.id.firstName);
+        EditText lastname = (EditText) findViewById(R.id.lastName);
+        EditText email = (EditText) findViewById(R.id.email);
+
+
         SharedPreferences myPrefsFile = getSharedPreferences("MyPrefsFile", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = myPrefsFile.edit();
-        editor.putString("firstName", put_firstName);
-        editor.putString("lastName", put_lastName);
-        editor.putString("Email", put_Email);
+        editor.putString("firstname", String.valueOf(firstname.getText()));
+        editor.putString("lastname", String.valueOf(lastname.getText()));
+        editor.putString("email", String.valueOf(email.getText()));
         editor.commit();
-        }*/
+        }
     }
